24e9227b81c4c59b0fb1dd51e468356a6f1a68b6
Imtiaz Miah <imtiazmiah@gmail.com>
2021-04-21 20:44:59 +0000

    Added AJAX for confirming tweets go to server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,32 +6,19 @@
 
 
 $(document).ready(function () {
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    console.log($('form').serialize());
+    $.ajax({
+      url:"/tweets",
+      method:"POST",
+      data: $('form').serialize()
+    }).then(function(response){
+      console.log(response)
+    })
+    })
   
-  // Database of existing tweets
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
   // Function to create tweets
   const createTweetElement = function (tweetObject) {
@@ -70,6 +57,6 @@ $(document).ready(function () {
     }
   }
 
-  renderTweets(data);
+  // renderTweets(data);
 
 });
