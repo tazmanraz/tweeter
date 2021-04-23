fc9208016e614e89ef4ee7111a222945740ff66c
Imtiaz Miah <imtiazmiah@gmail.com>
2021-04-23 04:28:41 +0000

    Added media-queries CSS for different screen sizes

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,108 +7,120 @@
 
 $(document).ready(function() {
 
-  // Cross Site Scripting - NOT FINISHED
-  const escape = function (str) {
+  //////////////////////////////////////
+  /////// HELPER FUNCTIONS /////////////
+  //////////////////////////////////////
+
+  // These helper functions will be moved to separate scripts with more time for best practises and readability
+
+  // Cross Site Scripting function To ensure no malicious postings get through to ruin web page
+  const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
+    // Function to create tweets as an HTML element
+    // escape function used in <p> tag to prevent malicious cross site scripting
+    const createTweetElement = function(tweetObject) {
+      const $tweet = $(`<article class="tweet">
+      <header>
+      <div class="tweet-header">
+        <div>
+          <img src="${tweetObject.user.avatars}" alt="user-avatar">
+          <span>${tweetObject.user.name}</span>
+        </div>
+        <span>${tweetObject.user.handle}</span>
+      </div>
+      <p class="tweet-message">${escape(tweetObject.content.text)}</p>
+    </header>
+    
+    </div>  <footer class="tweet-footer">
+  
+      <span>${timeago.format(tweetObject.created_at)}</span>
+  
+      <div class="footer-icons">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer>
+      </article>
+      `);
+      return $tweet;
+    }
+  
+    //Renders all tweets from database using createTweetElement
+    const renderTweets = function(tweets) {
+      // We must empty the container each time to avoid duplication
+      $('#tweets-container').empty();
+      for (const tweet of tweets) {
+        $('#tweets-container').prepend(createTweetElement(tweet));
+      }
+    }
+  
+    // Loading tweets
+    const loadTweets = function() {
+      $
+        .ajax('/tweets')
+        .then(function(tweets) {
+          renderTweets(tweets);
+        }).catch(function(error) {
+          console.error(error);
+        })
+    }
+
+  ///////////////////////////////////
+  /// CLIENT SUBMISSION WITH AJAX ///
+  ///////////////////////////////////
+  
+
   // Submitting Tweets from form submission
   $('form').on('submit', function(event) {
-    event.preventDefault()
+    //Ensures page doesn't refresh by default
+    event.preventDefault();
 
-    let charCount =  $('.tweet-post').val().length;
-    
-    // If statements checking for valid post submissions
+    let charCount = $('.tweet-post').val().length;
+
+    // If statements checking for valid post submissions. May move to another script
     if (charCount === 0) {
+      // Sliding up in case back to back errors
       $('.validation-text').slideUp();
 
-      $('.validation-text').html('Cannot send an empty tweet.')
-      .css('color', 'red')
+      // CSS hardcoded in here. Use jQuery addClass/removeClass features and put in stylesheet
+      // To add boxes and a more elegant looking error message.
+      $('.validation-text').html('🛑 CANNOT SEND AN EMPTY TWEET 🛑')
+        .css('color', 'red')
+        .slideDown(500);
 
-      .slideDown(500);
-      //return false;
     } else if (charCount > 140) {
       $('.validation-text').slideUp();
+      
       $('.validation-text')
-      .html('Please use less than 140 characters')
-      .css('color', 'red')
-
-      .slideDown(500);
+        .html('🛑 SEND LESS THAN 140 CHARACTERS 🛑')
+        .css('color', 'red')
+        .slideDown(500);
 
     } else {
-
       $('.validation-text').css('color', 'black').slideUp();
-    
-      // $('.validation-text').slideUp(500, function() { $(this).remove()})
 
-    $.ajax({
-      url:"/tweets",
-      method:"POST",
-      data: $('form').serialize() // should work with event.target.value - Test later for interest
-    
-    // Adding to page
-    }).then(function(response) {
-      $('.counter').val(140);
-      $('.tweet-post').val('');
-      loadTweets();
-      //console.log(loadTweets())
-    }).catch(function(error){
-      //console.error(error);
-    })
-  }
-  })
-  
-  // Function to create tweets
-  const createTweetElement = function (tweetObject) {
-    const $tweet = $(`<article class="tweet">
-    <header>
-    <div class="tweet-header">
-      <div>
-        <img src="${tweetObject.user.avatars}" alt="user-avatar">
-        <span>${tweetObject.user.name}</span>
-      </div>
-      <span>${tweetObject.user.handle}</span>
-    </div>
-    <p class="tweet-message">${escape(tweetObject.content.text)}</p>
-    <hr>
-  </header>
-  
-  </div>  <footer class="tweet-footer">
+      $.ajax({
+        url: "/tweets",
+        method: "POST",
+        data: $('form').serialize()
 
-    <span>${timeago.format(tweetObject.created_at)}</span>
-
-    <div class="footer-icons">
-      <i class="fas fa-flag"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="fas fa-heart"></i>
-    </div>
-  </footer>
-    </article>
-    `);
-    return $tweet;
-  }
-
-  //Renders all tweets from database using createTweetElement
-  const renderTweets = function(tweets) {
-    $('#tweets-container').empty();
-    for (const tweet of tweets) {
-      $('#tweets-container').prepend(createTweetElement(tweet));
+      // Adds to tweet page and resets all parameters in tweet submission box
+      }).then(function() {
+        $('.counter').val(140);
+        $('.tweet-post').val('');
+        loadTweets();
+      }).catch(function(error) {
+        console.error(error);
+      })
     }
-  }
+  })
 
-  // Loading tweets
-  const loadTweets = function() {
-    $
-    .ajax('/tweets')
-    .then(function(tweets) {
-      renderTweets(tweets);
-    }).catch(function(error) {
-      //console.error(error);
-    })
-  }
-
+  // Ensures initial tweets are loaded
   loadTweets();
 
 });
